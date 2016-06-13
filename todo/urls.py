89da11009d000bf8e6f86554065eefd976ba01c8
christine babu <temozex@gmail.com>
2016-06-13 18:56:33 -0400

    Associate root dir with index.html. Modify views by adding index function, uses render to display page

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from collection import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
