00c763fd8f2d9d13362eb750cf3c3c4598db7d37
christine babu <temozex@gmail.com>
2016-06-13 19:26:29 -0400

    Modify the modelspy for the database tables.

--- collection/models.py
+++ collection/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class todo(models.Model): #Table name, has to wrap models.Model to get the functionality of Django.
+         
+    name = models.CharField(max_length=100, unique=True) #Like a VARCHAR field
+    description = models.TextField() #Like a TEXT field
+    created = models.DateTimeField() #Like a DATETIME field
+ 
+    def __unicode__(self): #Tell it to return as a unicode string (The name of the to-do item) rather than just Object.
+        return self.name
