e9c5c7abae2db5c6933d3004882eba5a1e408a1b
christine babu <temozex@gmail.com>
2016-06-13 20:59:09 -0400

    Grant admin access to model. Modify index for different display on index.

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# import your model
+from collection.models import todo
+
+# and register it
+admin.site.register(todo)
--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,4 @@
+from models import todo
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,15 @@ from django.shortcuts import render
 def index(request):
     # this is your new view
     return render(request, 'index.html')
+
+#############################################################################
+# from models import todo
+from django.shortcuts import render_to_response
+ 
+def index(request): #Define our function, accept a request
+ 
+    items = todo.objects.all() #ORM queries the database for all of the to-do entries.
+    
+    #Responds with passing the object items (contains info from the DB) 
+    #to the template index.html
+    return render_to_response('index.html', {'items': items}) 
